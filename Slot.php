9356193285fe2488623080fba977f8e7e3ec35ec
Patrick Rütter <patrickruetter@gmail.com>
2015-02-06 11:38:50 +0100

    initial commit

---
+++ Slot.php
@@ -0,0 +1,36 @@
+<?php
+namespace Plugin\BreadcrumbSlot;
+
+class Slot {
+  public static function breadcrumbs($params) {
+    $defaultParams = array(
+      'pageId' => ipContent()->getCurrentPage()->getId(),
+      'separator' => ' › ',
+      'direction' => 'up'
+    );
+
+    $params = array_merge($defaultParams, $params);
+
+    $menuItems = array();
+
+    $currentPage = new \Ip\Page($params['pageId']);
+
+    while($currentPage->getParentId()) {
+      if($parentId = $currentPage->getParentId()) {
+        array_push($menuItems, $currentPage);
+        $currentPage = new \Ip\Page($parentId);
+      }
+    }
+
+    if($params['direction'] == 'down') {
+      $menuItems = array_reverse($menuItems);
+    }
+
+    $getTitleArray = function($value) {return $value->getTitle();};
+    $titleArray = array_map($getTitleArray, $menuItems);
+
+    return implode($params['separator'], $titleArray);
+
+  }
+
+}
