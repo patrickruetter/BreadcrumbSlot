1b6ba0b794dfb79285df2313e097b12479a6bcb2
Patrick Rütter <patrickruetter@gmail.com>
2015-02-06 13:53:16 +0100

    fix indentation

--- Slot.php
+++ Slot.php
@@ -2,37 +2,37 @@
 namespace Plugin\BreadcrumbSlot;
 
 class Slot {
-  private static $params = null;
-  public static function breadcrumbs($params) {
-    $defaultParams = array(
-        'pageId' => ipContent()->getCurrentPage()->getId(),
-        'separator' => ' › ',
-        'direction' => 'down',
-        'mode' => 'text'
-    );
+    private static $params = null;
+    public static function breadcrumbs($params) {
+        $defaultParams = array(
+            'pageId' => ipContent()->getCurrentPage()->getId(),
+            'separator' => ' › ',
+            'direction' => 'down',
+            'mode' => 'text'
+        );
 
-    self::$params = array_merge($defaultParams, $params);
+        self::$params = array_merge($defaultParams, $params);
 
-    $menuItemsArray = array();
+        $menuItemsArray = array();
 
-    $currentPage = new \Ip\Page($params['pageId']);
+        $currentPage = new \Ip\Page($params['pageId']);
 
-    while($currentPage->getParentId()) {
-      if($parentId = $currentPage->getParentId()) {
-        array_push($menuItemsArray, $currentPage);
-        $currentPage = new \Ip\Page($parentId);
-      }
+        while($currentPage->getParentId()) {
+            if($parentId = $currentPage->getParentId()) {
+                array_push($menuItemsArray, $currentPage);
+                $currentPage = new \Ip\Page($parentId);
+            }
+        }
+
+        if($params['direction'] == 'down') {
+            $menuItemsArray = array_reverse($menuItemsArray);
+        }
+
+        $breadcrumbArray = self::getBreadcrumbArray($menuItemsArray);
+
+        return implode($params['separator'], $breadcrumbArray);
+
     }
-
-    if($params['direction'] == 'down') {
-      $menuItemsArray = array_reverse($menuItemsArray);
-    }
-
-    $breadcrumbArray = self::getBreadcrumbArray($menuItemsArray);
-
-    return implode($params['separator'], $breadcrumbArray);
-
-  }
 
     static function getBreadcrumbArray($menuItemsArray) {
         $breadcrumbArray = array();
