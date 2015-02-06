3d5a6c860c5d0ba6ac61e3f09499fe9a7f31412e
Patrick Rütter <patrickruetter@gmail.com>
2015-02-06 13:52:23 +0100

    add html mode

--- Slot.php
+++ Slot.php
@@ -2,35 +2,54 @@
 namespace Plugin\BreadcrumbSlot;
 
 class Slot {
+  private static $params = null;
   public static function breadcrumbs($params) {
     $defaultParams = array(
-      'pageId' => ipContent()->getCurrentPage()->getId(),
-      'separator' => ' › ',
-      'direction' => 'down'
+        'pageId' => ipContent()->getCurrentPage()->getId(),
+        'separator' => ' › ',
+        'direction' => 'down',
+        'mode' => 'text'
     );
 
-    $params = array_merge($defaultParams, $params);
+    self::$params = array_merge($defaultParams, $params);
 
-    $menuItems = array();
+    $menuItemsArray = array();
 
     $currentPage = new \Ip\Page($params['pageId']);
 
     while($currentPage->getParentId()) {
       if($parentId = $currentPage->getParentId()) {
-        array_push($menuItems, $currentPage);
+        array_push($menuItemsArray, $currentPage);
         $currentPage = new \Ip\Page($parentId);
       }
     }
 
     if($params['direction'] == 'down') {
-      $menuItems = array_reverse($menuItems);
+      $menuItemsArray = array_reverse($menuItemsArray);
     }
 
-    $getTitleArray = function($value) {return $value->getTitle();};
-    $titleArray = array_map($getTitleArray, $menuItems);
+    $breadcrumbArray = self::getBreadcrumbArray($menuItemsArray);
 
-    return implode($params['separator'], $titleArray);
+    return implode($params['separator'], $breadcrumbArray);
 
   }
+
+    static function getBreadcrumbArray($menuItemsArray) {
+        $breadcrumbArray = array();
+        foreach($menuItemsArray as $menuItem) {
+            $mode = self::$params['mode'];
+
+            if($mode == 'text') {
+                array_push($breadcrumbArray, $menuItem->getTitle());
+            }
+            else if($mode == 'html') {
+                $lastClass = ($menuItem === end($menuItemsArray) ? 'class="last"' : '');
+
+                $string = '<a href="'.$menuItem->getLink().'" '.$lastClass.' >'.$menuItem->getTitle().'</a>';
+                array_push($breadcrumbArray, $string);
+            }
+        }
+        return $breadcrumbArray;
+    }
 
 }
